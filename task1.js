ef53787be51662727c34a381d47b91d9a166c863
ArgonDragonConstellation <133087494+ArgonDragonConstellation@users.noreply.github.com>
2024-06-06 02:30:47 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,24 +1,17 @@
 function findMin() {
     let a = parseFloat(document.getElementById('num1').value);
     let b = parseFloat(document.getElementById('num2').value);
-    let result = Math.min(a, b);
-    document.getElementById('result').textContent = Минимум: ${result};
+    document.getElementById('result').textContent = Минимум: ${Math.min(a, b)};
 }
 
 function findMax() {
     let a = parseFloat(document.getElementById('num1').value);
     let b = parseFloat(document.getElementById('num2').value);
-    let result = Math.max(a, b);
-    document.getElementById('result').textContent = Максимум: ${result};
+    document.getElementById('result').textContent = Максимум: ${Math.max(a, b)};
 }
 
 function checkEqual() {
     let a = parseFloat(document.getElementById('num1').value);
     let b = parseFloat(document.getElementById('num2').value);
-    let result = a === b;
-    if (result) {
-        document.getElementById('result').textContent = Числа равны: ${result}. Поздравляем, числа равны! 🥳;
-    } else {
-        document.getElementById('result').textContent = Упс, равенства нет 😢;
-    }
+    document.getElementById('result').textContent = Числа равны: ${a === b};
 }
