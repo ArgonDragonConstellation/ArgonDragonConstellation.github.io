c2ac02699a54a9de485132cc8c73ff8198fece10
ArgonDragonConstellation <133087494+ArgonDragonConstellation@users.noreply.github.com>
2024-06-06 02:34:01 +0300

    Delete task3.js

--- task3.js
+++
@@ -1,7 +0,0 @@
-function askAge() {
-    let age;
-    do {
-        age = prompt("Пожалуйста, введите ваш возраст:");
-    } while (age === null || age.trim() === "");
-    alert(Ваш возраст: ${age});
-}
